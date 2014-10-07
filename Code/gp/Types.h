ad8013241d5079cc129fdfd5fae8c1a437c5d2e4
Manuel Maier <mjmaier@gmx.de>
2014-10-07 09:51:53 +0200

    Added using templates for Ptr, Ref, etc.

--- Code/gp/Types.h
+++ Code/gp/Types.h
@@ -1,3 +1,24 @@
 #pragma once
 
+template<typename T>
+using Ptr = T*;
+
+template<typename T>
+using Ref = T&;
+
+template<typename T>
+using Const = T const;
+
+template<typename T>
+using Volatile = T volatile;
+
+template<typename CharacterType = char>
+using StringData = Ptr<Const<CharacterType>>;
+
+template<typename CharacterType = char>
+using StringBuffer = Ptr<CharacterType>;
+
+#define AddressOf(x) (&x)
+#define Deref(x) (*x)
+
 using Scalar = float;
