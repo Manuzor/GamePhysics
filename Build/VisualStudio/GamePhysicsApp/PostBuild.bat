efdce3df993b3bd960b94bc249210304b6a0725e
Manuel Maier <mjmaier@gmx.de>
2014-10-11 09:34:42 +0200

    Small fix in post build script

--- Build/VisualStudio/GamePhysicsApp/PostBuild.bat
+++ Build/VisualStudio/GamePhysicsApp/PostBuild.bat
@@ -1,3 +1,3 @@
 @echo off
 
-python3 "%GP_Tool_CopyDependencies% " "%~1 " "%GP_Config% " "%GP_Architecture% " "%CD%\Dependencies.py" -f
+python3 "%GP_Tool_CopyDependencies% " "%~1 " "%GP_Config% " "%GP_Architecture% " "%CD%\Dependencies.py"
