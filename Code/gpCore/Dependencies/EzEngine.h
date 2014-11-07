64c6bff5fd83c47dd0750ca87118d28509baf621
Manuel Maier <mjmaier@gmx.de>
2014-11-07 11:52:08 +0100

    Adjusted to new ezEngine version

--- Code/gpCore/Dependencies/EzEngine.h
+++ Code/gpCore/Dependencies/EzEngine.h
@@ -3,9 +3,7 @@
 #include <Core/PCH.h>
 #include <CoreUtils/PCH.h>
 #include <GameUtils/PCH.h>
+
 #include <Foundation/Math/Rect.h>
-#include <Foundation/Communication/GlobalEvent.h>
-#include <Foundation/Threading/ThreadSignal.h>
 #include <Foundation/Math/Size.h>
-#include <Foundation/Basics/Types/RefCounted.h>
 #include <Foundation/Configuration/CVar.h>
